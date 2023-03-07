a0704ad2be9a4b7ffcc39c521b9ac14cb8d47061
Jorge Vacas Prieto <jorge.vacas.prieto@gmail.com>
2023-03-07 12:31:43 +0100

    First commit

---
+++ tacos-shop/src/main/java/edu/shop/tacos/data/OrderRepository.java
@@ -0,0 +1,9 @@
+package edu.shop.tacos.data;
+
+import edu.shop.tacos.model.Order;
+
+public interface OrderRepository {
+
+	Order save (Order order);
+}
+
---
+++ tacos-shop/src/test/java/edu/shop/tacos/TacosShopApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.shop.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacosShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
